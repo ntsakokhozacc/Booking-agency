d7dd5d7fae0798b72ecc681e5a835ca942dfc8a2
ntsako <ntsakokhozacc@gmail.com>
2021-09-14 01:23:08 +0200

    ft-logout for users

--- telusko/accounts/urls.py
+++ telusko/accounts/urls.py
@@ -1,9 +1,13 @@
+from django import VERSION
 from django.urls import path
 
 from . import views 
 #url to every page in use within the website
+
 urlpatterns=[
     path("register",views.register, name="register"),
     path("login",views.login, name="login"),
-    path("exit",views.exit, name="exit")
+
+    #Comment out logotu feature to run the project
+    path("logout",views.logout, name="logout")
 ]
